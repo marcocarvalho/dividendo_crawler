d2604182ed85f0c4fbd8054df27f88569c03ffb7
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2023-03-15 17:09:55 -0300

    Download file

--- lib/dividendo_crawler/rad_cvm.rb
+++ lib/dividendo_crawler/rad_cvm.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class DividendoCrawler::RadCVM < DividendoCrawler::Base
+  def self.save(file, url)
+    response = new.fetch(url)
+
+    if file.respond_to?(:write)
+      file.write(response.body)
+    else
+      File.write(file, response.body)
+    end
+  end
+
   def fetch(url)
     connection(url).get
   end
